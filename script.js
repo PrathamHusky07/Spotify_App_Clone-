53eb1d248028024f125c6beb1edf2a25f16cd25d
PrathamHusky07 <kulkarni.prath@northeastern.edu>
2023-01-17 19:26:37 -0500

    Minor changes in the design

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ console.log("Welcome to Spotify")
 let songIndex = 0;
 let audioElement = new Audio('Songs/castleofglass.mp3');
 let masterPlay = document.getElementById('masterPlay');
-let myDisplayBar = document.getElementById('myDisplayBar');    
+let myDisplayBar = document.getElementById('myDisplayBar');
+let gif = document.getElementById('gif');    
 
 let songs = [
     {songName: 'Castle of Glass', filepath: "Songs/castleofglass.mp3", coverPath: "lp2.jpg"},
@@ -24,14 +25,24 @@ masterPlay.addEventListener('click', ()=> {
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle')
+        gif.style.opacity = 1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle')
+        gif.style.opacity = 0;
         
     }
 })
-myDisplayBar.addEventListener('timeupdate', ()=> {
-    console.log('timeupdate');
+audioElement.addEventListener('timeupdate', ()=> {
+    
+    progress = parseInt((audioElement.currentTime/audioElement.duration) * 100);
+    myDisplayBar.value = progress;
+
+})
+
+myDisplayBar.addEventListener('change', ()=> {
+    audioElement.currentTime = myDisplayBar.value * audioElement.duration/100;
+
 })
